559f4f489c5d53fab541961b33dcfaddb6bbb9e2
Adrian Gonzalez Meda <adriangonzalezmeda@gmail.com>
2024-11-21 12:55:38 +0100

    fetch meal and trigger not found page

--- 19. NextJS/The foodies app/app/meals/[mealSlug]/page.js
+++ 19. NextJS/The foodies app/app/meals/[mealSlug]/page.js
@@ -1,10 +1,42 @@
-const MealDetailPage = ({ params }) => {
+import Image from 'next/image';
+import classes from './page.module.css';
+import { getMeal } from '@/lib/meals';
+import { notFound } from 'next/navigation';
+
+// params is provided by NextJS with the params of the url in a key: value way. The key its the folder name [mealSlug]
+const MealDetailsPage = ({ params }) => {
+    const meal = getMeal(params.mealSlug);
+
+    if (!meal) {
+        // Trigger the closest not found error page
+        notFound();
+    }
+
+    meal.instructions = meal.instructions.replace(/\n/g, '</br>');
+
     return (
-        <main>
-            <h1>Meal Detail</h1>
-            {params.mealSlug}
-        </main>
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={meal.image} fill />
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{ meal.title }</h1>
+                    <p className={classes.creator}>
+                        by <a href={`mailto:${meal.creator_email}`}>{ meal.creator }</a>
+                    </p>
+                    <p className={classes.summary}>{ meal.summary }</p>
+                </div>
+            </header>
+            <main className={classes.main}>
+                <p className={classes.instructions} dangerouslySetInnerHTML={{
+                    __html: meal.instructions
+                }}>
+                    {/* dangerouslySetInnerHTML: output html code, you expose to scripting attacks if not validate */}
+                </p>
+            </main>
+        </>
     )
 }
 
-export default MealDetailPage
+export default MealDetailsPage
--- 19. NextJS/The foodies app/app/meals/not-found.js
+++ 19. NextJS/The foodies app/app/meals/not-found.js
@@ -3,8 +3,8 @@
 const NotFound = () => {
     return (
         <main className="not-found">
-            <h1>Not found</h1>
-            <p>Unfortunately, we could not find the requested page or resource.</p>
+            <h1>Meal not found</h1>
+            <p>Unfortunately, we could not find the requested page or meal.</p>
         </main>
     )
 }
